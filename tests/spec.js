84d54b93fbf8600dac64c3a8838f0532b422842d
RubyLouvre <cheng19840218@gmail.com>
2014-03-05 17:57:43 +0800

    添加单元测试

--- tests/spec.js
+++ tests/spec.js
@@ -1,39 +1,19 @@
-define([],function() {
+define([], function() {
 
 
-    describe('Assertion', function() {
+    describe('isWindow', function() {
 
         it('be', function() {
-            expect(1).to.be(1)
-            expect('1').not.to.be(1)
-            expect(NaN).not.to.be(NaN)
+            expect(avalon.isWindow(1)).to.be(false)
+            expect(avalon.isWindow({})).to.be(false)
+            var obj = {
+            }
+            obj.window = obj
+            expect(avalon.isWindow(obj)).to.be(false)
+            expect(avalon.isWindow(window)).to.be(true)
         })
 
-        it('eql', function() {
-            expect('1').to.eql(1)
-            expect(1).to.eql(true)
-            expect({
-                a : 'b'
-            }).to.eql({
-                a : 'b'
-            })
-        })
-
-        it('ok', function() {
-            expect(1).to.be.ok()
-            expect({}).to.be.ok()
-            expect(0).not.to.be.ok()
-        })
-
-        it('a / an', function() {
-            expect(5).to.be.a('number')
-            expect([]).to.be.an('array')
-        })
-
-        it('match', function() {
-            expect('0.1.0').to.match(/[0-9]+\.[0-9]+\.[0-9]+/)
-        })
     })
 
-  
+
 })
