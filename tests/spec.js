8805a3eda7a325a77338a14d3d4d78f99f27a7e4
RubyLouvre <cheng19840218@gmail.com>
2014-03-05 20:38:56 +0800

    update test

--- tests/spec.js
+++ tests/spec.js
@@ -102,6 +102,7 @@ define([], function() {
     })
 
     describe('range', function() {
+        
         it('be', function() {
             expect(avalon.range(10)).to.eql([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
             expect(avalon.range(1, 11)).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
@@ -109,9 +110,11 @@ define([], function() {
             expect(avalon.range(0, -10, -1)).to.eql([0, -1, -2, -3, -4, -5, -6, -7, -8, -9])
             expect(avalon.range(0)).to.eql([])
         })
+        
     })
 
     describe('oneObject', function() {
+        
         it('be', function() {
             expect(avalon.oneObject("aa,bb,cc")).to.eql({
                 "aa": 1,
@@ -124,26 +127,45 @@ define([], function() {
                 "3": false
             })
         })
+        
     })
 
     describe('slice', function() {
+        
         it('be', function() {
-            var a = [1, 2, 3, 4, 5, 6, 7];
-            expect(avalon.slice(a, 0)).to.eql(a.slice(0));
-            expect(avalon.slice(a, 1, 4)).to.eql(a.slice(1, 4));
-            expect(avalon.slice(a, -1)).to.eql(a.slice(-1));
-            expect(avalon.slice(a, 1, -2)).to.eql(a.slice(1, -2));
-            expect(avalon.slice(a, 1, NaN)).to.eql(a.slice(1, NaN));
-            expect(avalon.slice(a, 1, 2.1)).to.eql(a.slice(1, 2.1));
-            expect(avalon.slice(a, 1.1, 4)).to.eql(a.slice(1.1, 4));
-            expect(avalon.slice(a, 1.2, NaN)).to.eql(a.slice(1, NaN));
-            expect(avalon.slice(a, NaN)).to.eql(a.slice(NaN));
-            expect(avalon.slice(a, 1.3, 3.1)).to.eql(a.slice(1.3, 3.1));
-            expect(avalon.slice(a, 2, "XXX")).to.eql(a.slice(2, "XXX"));
-            expect(avalon.slice(a, -2)).to.eql(a.slice(-2));
-            expect(avalon.slice(a, 1, 9)).to.eql(a.slice(1, 9));
-            expect(avalon.slice(a, 20, -21)).to.eql(a.slice(20, -21));
-            expect(avalon.slice(a, -1, null)).to.eql(a.slice(-1, null));
+            var a = [1, 2, 3, 4, 5, 6, 7]
+            expect(avalon.slice(a, 0)).to.eql(a.slice(0))
+            expect(avalon.slice(a, 1, 4)).to.eql(a.slice(1, 4))
+            expect(avalon.slice(a, -1)).to.eql(a.slice(-1))
+            expect(avalon.slice(a, 1, -2)).to.eql(a.slice(1, -2))
+            expect(avalon.slice(a, 1, NaN)).to.eql(a.slice(1, NaN))
+            expect(avalon.slice(a, 1, 2.1)).to.eql(a.slice(1, 2.1))
+            expect(avalon.slice(a, 1.1, 4)).to.eql(a.slice(1.1, 4))
+            expect(avalon.slice(a, 1.2, NaN)).to.eql(a.slice(1, NaN))
+            expect(avalon.slice(a, NaN)).to.eql(a.slice(NaN))
+            expect(avalon.slice(a, 1.3, 3.1)).to.eql(a.slice(1.3, 3.1))
+            expect(avalon.slice(a, 2, "XXX")).to.eql(a.slice(2, "XXX"))
+            expect(avalon.slice(a, -2)).to.eql(a.slice(-2))
+            expect(avalon.slice(a, 1, 9)).to.eql(a.slice(1, 9))
+            expect(avalon.slice(a, 20, -21)).to.eql(a.slice(20, -21))
+            expect(avalon.slice(a, -1, null)).to.eql(a.slice(-1, null))
+        })
+        
+    })
+
+    describe('textNode.nodeValue === textNode.data', function() {
+        
+        it('be', function() {
+
+            var element = document.createElement("div")
+            element.innerHTML = "zzzz<!--yyy-->"
+            document.body.appendChild(element)
+            expect(element.firstChild.nodeType).to.be(3)
+            expect(element.lastChild.nodeType).to.be(8)
+            expect(element.firstChild.nodeValue).to.be(element.firstChild.data)
+            expect(element.lastChild.nodeValue).to.be(element.lastChild.data)
+            document.body.removeChild(element)
+            
         })
     })
 
