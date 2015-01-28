41aa1e1bec993bbf1d96f5f7150e4e9f79f40d13
RubyLouvre <cheng19840218@gmail.com>
2015-01-28 19:50:26 +0800

    update

--- tests/spec.js
+++ tests/spec.js
@@ -140,21 +140,25 @@ define([], function() {
                 })
             })
         })
-        it("测试text插件", function(done) {
+        it("测试text,css插件", function(done) {
             setTimeout(function() {
                 require.config({
                     baseUrl: "/avalon/src/jQuery/"
                 })
                 var index = 0
-                require(["./aaa.js", "text!aaa.txt"], function(a, b) {
+                require(["./aaa.js", "text!./aaa.txt", "css!./eee"], function(a, b) {
                     expect(a).to.be("aaa")
                     expect(b).to.be("text")
                     ++index
                 })
                 setTimeout(function() {
                     expect(index).to.be(1)
+                    var color = avalon(document.body).css("border-left-color")
+                    if (color === "rgb(255, 0, 0)")
+                        color = "red"
+                    expect(color).to.be("red")
                     done()
-                }, 150)
+                }, 140)
             })
         })
 
