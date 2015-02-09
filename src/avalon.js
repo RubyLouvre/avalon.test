595cb72f91ffbd16c6a2b082b0d12a290e863601
RubyLouvre <cheng19840218@gmail.com>
2015-02-09 16:56:40 +0800

    update

--- src/avalon.js
+++ src/avalon.js
@@ -5,7 +5,7 @@
  http://weibo.com/jslouvre/
  
  Released under the MIT license
- avalon.js 1.391 build in 2015.2.7 
+ avalon.js 1.391 build in 2015.2.9 
 _____________________________________
  support IE6+ and other browsers
  ==================================================*/
@@ -38,7 +38,7 @@ var expose = new Date - 0
 //http://stackoverflow.com/questions/7290086/javascript-use-strict-and-nicks-find-global-function
 var DOC = window.document
 var head = DOC.getElementsByTagName("head")[0] //HEAD元素
-var ifGroup = head.insertBefore(document.createElement("avalon"), head.firstChild) //避免IE6 base标签BUG
+var ifGroup = head.insertBefore(document.createElement("base"), head.firstChild) //避免IE6 base标签BUG
 ifGroup.innerHTML = "X<style id='avalonStyle'>.avalonHide{ display: none!important }</style>"
 ifGroup.setAttribute("ms-skip", "1")
 var rnative = /\[native code\]/ //判定是否原生函数
@@ -4774,7 +4774,7 @@ new function() {
         }
         module = modules[urlNoQuery]
         if (module && module.state >= 3) {
-            require(module.deps, module.factory, urlNoQuery)
+            innerRequire(module.deps, module.factory, urlNoQuery)
             return urlNoQuery
         }
         if (name && !module) {
@@ -4865,20 +4865,20 @@ new function() {
 
     //核心API之二 require
     innerRequire.define = function(name, deps, factory) { //模块名,依赖列表,模块本身
-        var args = aslice.call(arguments)
         if (typeof name !== "string") {
-            args.unshift("anonymous")
+            factory = deps
+            deps = name
+            name = "anonymous"
         }
-
-        if (!Array.isArray(args[1])) {
-            args.splice(1, 0, [])
+        if (!Array.isArray(deps)) {
+            factory = deps
+            deps = []
         }
         var config = {
             built: !isUserFirstRequire, //用r.js打包后,所有define会放到requirejs之前
             defineName: name
         }
-        factory = args[2]
-        args = [args[1], factory, config]
+        var args = [deps, factory, config]
         factory.require = function(url) {
             args.splice(2, 0, url)
             if (modules[url]) {
